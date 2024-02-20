3be973ad778b2fbaa28cc1c828f30abd4c69d86d
Lendly Cagata <lendly.cagata@telus.com>
2024-02-20 23:05:07 +0800

    fix: bypass --no-source

--- terraform/modules/fourkeys/cloud-run-events-bq-writer.tf
+++ terraform/modules/fourkeys/cloud-run-events-bq-writer.tf
@@ -8,7 +8,7 @@ module "gcloud_build_batch_events_bq_writer" {
   source                 = "terraform-google-modules/gcloud/google"
   version                = "~> 2.0"
   create_cmd_entrypoint  = "gcloud"
-  create_cmd_body        = "builds submit ../batch_events --tag=${local.events_bq_writer_container_url}:${data.archive_file.events_bq_writer_source.output_sha} --project=${var.project_id}"
+  create_cmd_body        = "builds submit ../batch_events --tag=${local.events_bq_writer_container_url}:${data.archive_file.events_bq_writer_source.output_sha} --project=${var.project_id} --no-source"
   destroy_cmd_entrypoint = "gcloud"
   destroy_cmd_body       = "container images delete ${local.events_bq_writer_container_url}:${data.archive_file.events_bq_writer_source.output_sha} --quiet"
 }
