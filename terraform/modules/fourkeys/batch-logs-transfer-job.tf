9382f1aec3bb480317834076f0b89932139af2cd
Lendly Cagata <lendly.cagata@telus.com>
2024-02-22 08:30:33 +0800

    fix: sdsdsdsdsdsdsdsdsdsdsdsd

--- terraform/modules/fourkeys/batch-logs-transfer-job.tf
+++ terraform/modules/fourkeys/batch-logs-transfer-job.tf
@@ -1,43 +1,43 @@
-# data "google_storage_transfer_project_service_account" "logs" {
-#   project = var.project_id
-# }
+data "google_storage_transfer_project_service_account" "logs" {
+  project = var.project_id
+}
 
-# resource "google_storage_transfer_job" "batch_logs_transfer_job" {
-#   description = "This will transfer GitHub LOGS in archive bucket every hour"
-#   project     = var.project_id
+resource "google_storage_transfer_job" "batch_logs_transfer_job" {
+  description = "This will transfer GitHub LOGS in archive bucket every hour"
+  project     = var.project_id
 
-#   schedule {
-#     schedule_start_date {
-#       year  = 2024
-#       month = 2
-#       day   = 21
-#     }
+  schedule {
+    schedule_start_date {
+      year  = 2024
+      month = 2
+      day   = 21
+    }
 
-#     repeat_interval = "3600s" # Every hour
-#   }
+    repeat_interval = "3600s" # Every hour
+  }
 
-#   transfer_spec {
-#     gcs_data_source {
-#       bucket_name = "off-net-dev-gh-audit-log-station-local"
-#       path        = "fourkeys/audit-logs/"
-#     }
+  transfer_spec {
+    gcs_data_source {
+      bucket_name = "off-net-dev-gh-audit-log-station-local"
+      path        = "fourkeys/audit-logs/"
+    }
 
-#     gcs_data_sink {
-#       bucket_name = "off-net-dev-gh-audit-log-archieve-local"
-#     }
-#   }
-# }
+    gcs_data_sink {
+      bucket_name = "off-net-dev-gh-audit-log-archieve-local"
+    }
+  }
+}
 
-# resource "google_storage_bucket_iam_member" "source_bucket_access_logs" {
-#   bucket = "off-net-dev-gh-audit-log-station-local"
-#   role   = "roles/storage.objectViewer"
-#   member = "serviceAccount:dora-wif@off-net-dev.iam.gserviceaccount.com"
-# }
+resource "google_storage_bucket_iam_member" "source_bucket_access_logs" {
+  bucket = "off-net-dev-gh-audit-log-station-local"
+  role   = "roles/storage.objectViewer"
+  member = "serviceAccount:dora-wif@off-net-dev.iam.gserviceaccount.com"
+}
 
-# resource "google_storage_bucket_iam_member" "destination_bucket_access_logs" {
-#   bucket = "off-net-dev-gh-audit-log-archieve-local"
-#   role   = "roles/storage.objectCreator"
-#   member = "serviceAccount:dora-wif@off-net-dev.iam.gserviceaccount.com"
-# }
+resource "google_storage_bucket_iam_member" "destination_bucket_access_logs" {
+  bucket = "off-net-dev-gh-audit-log-archieve-local"
+  role   = "roles/storage.objectCreator"
+  member = "serviceAccount:dora-wif@off-net-dev.iam.gserviceaccount.com"
+}
 
 
