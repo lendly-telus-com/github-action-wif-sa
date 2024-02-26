5fb5160307d59908ff7f60e50c459b7254f410a6
Lendly Cagata <71040527+lendly-telus-com@users.noreply.github.com>
2024-02-27 02:07:01 +0800

    Revert "fix: ghhhhhhhhhhhhhh"

--- terraform/modules/fourkeys/batch_gh_audit_log.tf
+++ terraform/modules/fourkeys/batch_gh_audit_log.tf
@@ -6,17 +6,34 @@ data "archive_file" "batch_gh_audit_log_source" {
 
 resource "google_storage_bucket_object" "batch_gh_audit_log_zip" {
   source       = data.archive_file.batch_gh_audit_log_source.output_path
+  content_type = "application/zip"
+
+  # Append to the MD5 checksum of the files's content
+  # to force the zip to be updated as soon as a change occurs
   name   = "src-${data.archive_file.batch_gh_audit_log_source.output_md5}.zip"
   bucket = google_storage_bucket.batch_gh_log_function_bucket.name
 }
 
-data "google_storage_project_service_account" "gcs_account" {}
+# data "google_storage_project_service_account" "gcs_account" {}
 
-resource "google_project_iam_member" "invoking" {
-  project = var.project_id
-  role    = "roles/run.invoker"
-  member  = "serviceAccount:dora-wif@off-net-dev.iam.gserviceaccount.com"
-}
+# resource "google_project_iam_member" "gcs-pubsub-publishing" {
+#   project = var.project_id
+#   role    = "roles/pubsub.publisher"
+#   member  = "serviceAccount:${data.google_storage_project_service_account.gcs_account.email_address}"
+# }
+
+# resource "google_service_account" "gh-audit-log-account" {
+#   account_id   = "gh-audit-log-sa"
+#   display_name = "GH Audit Log Service Account"
+# }
+
+# Permissions on the service account used by the function and Eventarc trigger
+# resource "google_project_iam_member" "invoking" {
+#   project = var.project_id
+#   role    = "roles/run.invoker"
+#   member  = "serviceAccount:dora-wif@off-net-dev.iam.gserviceaccount.com"
+#   depends_on = [google_project_iam_member.gcs-pubsub-publishing]
+# }
 
 resource "google_project_iam_member" "event-receiving" {
   project = var.project_id
