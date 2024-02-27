62148c3f542625249a4983504bdec29e2c369e96
Lendly Cagata <lendly.cagata@telus.com>
2024-02-27 23:47:43 +0800

    fix: source code issue

--- terraform/modules/fourkeys/batch_gh_audit_log.tf
+++ terraform/modules/fourkeys/batch_gh_audit_log.tf
@@ -1,19 +1,8 @@
-data "archive_file" "gh_log_cf" {
-  type        = "zip"
-  source_dir  = "../batch_gh_audit_log"
-  output_path = "/tmp/batch_gh_audit_log.zip"
+resource "google_storage_bucket" "function_bucket" {
+  name                        = "${var.project_id}-function-source"
+  location                    = var.region
+  uniform_bucket_level_access = true
 }
-
-module "gcloud_build_batch_gh_log_cf" {
-  source                 = "terraform-google-modules/gcloud/google"
-  version                = "~> 2.0"
-  create_cmd_entrypoint  = "gcloud"
-  create_cmd_body        = "builds submit ../batch_gh_audit_log --tag=${local.gh_log_cf_url}:${data.archive_file.gh_log_cf.output_sha} --project=${var.project_id} --gcs-log-dir=gs://tf-cloud-build-logs --config=/path/to/cloudbuild.yaml"
-  destroy_cmd_entrypoint = "gcloud"
-  destroy_cmd_body       = "container images delete ${local.gh_log_cf_url}:${data.archive_file.gh_log_cf.output_sha} --quiet"
-}
-
-
 
 resource "google_storage_bucket" "trigger-bucket" {
   name     = "batch-gh-log-audit-source-bucket"
@@ -21,16 +10,38 @@ resource "google_storage_bucket" "trigger-bucket" {
   uniform_bucket_level_access = true
 }
 
+data "archive_file" "gh_log_cf" {
+  type        = "zip"
+  source_dir  = "../batch_gh_audit_log"
+  output_path = "/tmp/batch_gh_audit_log.zip"
+}
+
+resource "google_storage_bucket_object" "load_gh_audit_log_zip" {
+  source       = data.archive_file.gh_log_cf.output_path
+  content_type = "application/zip"
+
+  # Append to the MD5 checksum of the files's content
+  # to force the zip to be updated as soon as a change occurs
+  name   = "src-${data.archive_file.gh_log_cf.output_md5}.zip"
+  bucket = google_storage_bucket.function_bucket.name
+}
 
 resource "google_cloudfunctions2_function" "batch_gh_audit_log_function" {
   name    = "batch_gh_audit_log"
   description = "Batch GH Audit Log"
   location = var.region
-  depends_on  = [module.gcloud_build_batch_gh_log_cf]
+  
 
   build_config {
     runtime = "python310"
     entry_point = "persist_data"
+
+    source {
+      storage_source {
+        bucket = google_storage_bucket.function_bucket.name
+        object = google_storage_bucket_object.load_gh_audit_log_zip.name
+      }
+    }
   }
 
   service_config {
