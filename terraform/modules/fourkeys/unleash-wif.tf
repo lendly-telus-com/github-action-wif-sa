324a720f2f6d1c1e85faa8bb9aa6f039847bf7c6
Lendly Cagata | Local Admin <lendly.cagata@telus.com>
2024-03-14 07:46:57 +0800

    fix: dfgasrg

--- terraform/modules/fourkeys/unleash-wif.tf
+++ terraform/modules/fourkeys/unleash-wif.tf
@@ -5,7 +5,7 @@ resource "google_service_account" "unleash_sa" {
 }
 
 resource "google_iam_workload_identity_pool_provider" "provider" {
-  workload_identity_pool_id          = google_iam_workload_identity_pool.pool.workload_identity_pool_id
+  # workload_identity_pool_id          = google_iam_workload_identity_pool.pool.workload_identity_pool_id
   workload_identity_pool_provider_id = "unleash-provider"
   attribute_mapping                  = {
     "google.subject" = "assertion.sub"
